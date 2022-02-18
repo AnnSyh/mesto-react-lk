5ac105ca287ddc8c580a3d4565a555025e5b6178
AnnSyh <ann.syharevskaya@gmail.com>
2022-02-18 18:36:05 +0300

    получила данные карточек по API

--- src/components/App.js
+++ src/components/App.js
@@ -6,39 +6,40 @@ import Profile from './profile/Profile';
 import PopupWithForm from './PopupWithForm/PopupWithForm';
 import ImgPopup from './imgPopup/imgPopup';
 import Card from './card/Card';
+import api from  '../utils/api';
 
-let cards = [
-  {
-    "id": "jaH3QF46gAY",
-    "src": "https://images.unsplash.com/photo-1513326738677-b964603b136d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw5NTI3OXwwfDF8c2VhcmNofDF8fFJ1c3NpYXxlbnwwfHx8fDE2NDQyNjQ3MDE&ixlib=rb-1.2.1&q=80&w=1080",
-    "alt": "Saint Basil's Cathedral, Moscow, Russia",
-    "title": "St. Basil’s Cathedral in Moscow",
-    "subtitle": "Nikolay Vorobyev"
-  },
-  {
-    "id": "L4jrg4c7928",
-    "src": "https://images.unsplash.com/photo-1547448415-e9f5b28e570d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw5NTI3OXwwfDF8c2VhcmNofDJ8fFJ1c3NpYXxlbnwwfHx8fDE2NDQyNjQ3MDE&ixlib=rb-1.2.1&q=80&w=1080",
-    "alt": "brown and gray concrete building during daytime",
-    "title": "Finally froze while looking for this shot, but it was worth it.",
-    "subtitle": "Michael Parulava"
-  },
-  {
-    "id": "lvJZhHOIJJ4",
-    "src": "https://images.unsplash.com/photo-1520106212299-d99c443e4568?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw5NTI3OXwwfDF8c2VhcmNofDN8fFJ1c3NpYXxlbnwwfHx8fDE2NDQyNjQ3MDE&ixlib=rb-1.2.1&q=80&w=1080",
-    "alt": "St. Basils Cathedral",
-    "title": "Saint Basil’s Cathedral",
-    "subtitle": "Nikita Karimov"
-  },
-];
-const addCardToDB = data => {
-  cards = [...cards, data];
-}
+// let cards = [
+//   {
+//     "id": "jaH3QF46gAY",
+//     "src": "https://images.unsplash.com/photo-1513326738677-b964603b136d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw5NTI3OXwwfDF8c2VhcmNofDF8fFJ1c3NpYXxlbnwwfHx8fDE2NDQyNjQ3MDE&ixlib=rb-1.2.1&q=80&w=1080",
+//     "alt": "Saint Basil's Cathedral, Moscow, Russia",
+//     "title": "St. Basil’s Cathedral in Moscow",
+//     "subtitle": "Nikolay Vorobyev"
+//   },
+//   {
+//     "id": "L4jrg4c7928",
+//     "src": "https://images.unsplash.com/photo-1547448415-e9f5b28e570d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw5NTI3OXwwfDF8c2VhcmNofDJ8fFJ1c3NpYXxlbnwwfHx8fDE2NDQyNjQ3MDE&ixlib=rb-1.2.1&q=80&w=1080",
+//     "alt": "brown and gray concrete building during daytime",
+//     "title": "Finally froze while looking for this shot, but it was worth it.",
+//     "subtitle": "Michael Parulava"
+//   },
+//   {
+//     "id": "lvJZhHOIJJ4",
+//     "src": "https://images.unsplash.com/photo-1520106212299-d99c443e4568?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw5NTI3OXwwfDF8c2VhcmNofDN8fFJ1c3NpYXxlbnwwfHx8fDE2NDQyNjQ3MDE&ixlib=rb-1.2.1&q=80&w=1080",
+//     "alt": "St. Basils Cathedral",
+//     "title": "Saint Basil’s Cathedral",
+//     "subtitle": "Nikita Karimov"
+//   },
+// ];
+// const addCardToDB = data => {
+//   cards = [...cards, data];
+// }
 
 function App() {
   //сохраняем ссылку на картинку вводимую в input(это для формы добавления эл-та)
   const [pictureLink, setPictureLink] = React.useState('https://cdn.fishki.net/upload/post/2021/02/02/3586907/tn/8-11.jpg');
-  //устанавливаем нач набор карточек из массива
-  const [cardData, setCardData] = React.useState(cards)
+  // //устанавливаем нач набор карточек из массива
+  // const [cardData, setCardData] = React.useState(cards)
 
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false)
   const [isAddPlacePopupOpen, setisAddPlacePopupOpen] = React.useState(false)
@@ -78,6 +79,21 @@ function App() {
     setisImgPopupOpen(false);
   };
 
+
+  const [cards , setCards ] = React.useState([]);
+
+  React.useEffect(() => {
+
+    api.getInitialCards()
+      .then((cards) => {
+          console.log('cards = ',cards)
+          setCards(cards);
+      })
+      .catch((err) => console.log(err));
+
+  }, []);
+
+
   return (
     <>
       <Header />
@@ -92,10 +108,13 @@ function App() {
         <section className="cards section content__section ">
           <div className="list-template-inner">
           <ul className="cards__list list-template-place">
-              {cardData.map(({ id, ...card }) => {
+              {cards.map(({ id, ...card }) => {
                   return (
-                    <Card  key={id} handleCardClick={() => handleCardClick(card)} {...card} />
-                  );
+                    <Card   key={card._id} 
+                            handleCardClick={() => handleCardClick(card)} 
+                            {...card} 
+                    />
+                        );
               })}
           </ul>
           </div>
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 
-function Card({ handleCardClick, title, src }) {
+// function Card({ handleCardClick, title, src }) {
+function Card(props) {
+    
+    console.log('Card props= ',props);
+    // console.log('Card props.link= ',props.link);
+    // console.log('Card props.name= ',props.name);
+
+    function handleCardClick() {
+        props.onCardClick(props);
+      }    
+
+
     return (
         <li className="cards__item">
             <div className="cards__pic" 
@@ -9,15 +20,15 @@ function Card({ handleCardClick, title, src }) {
              >
                 <img className="cards__img" 
                         data-popup="open-img__popup" 
-                        src={src}
-                        alt={title} /> 
+                        src={props.link}
+                        alt={props.name} /> 
                 <button className="cards__trash hidden"></button>
             </div>
             <div className="cards__text">
-                <h2 className="cards__title text-overflow">{title}</h2>
+                <h2 className="cards__title text-overflow">{props.name}</h2>
                 <div>
                     <button className="cards__heart"></button>
-                    <div className="cards__heart-counter">1</div>
+                    <div className="cards__heart-counter">{props.likes.length}</div>
                 </div>
             </div>
         </li>
